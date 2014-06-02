5014261c6dac338d71d6a314ebcf753af5323112
Jérôme Loï <gorhgorh@gmail.com>
2014-06-02 16:40:06 +0200

    added option for ports, and reset a a factory function

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-var five = require('johnny-five');
+var five = require('johnny-five'),
+    opt = require('node-getopt').create([
+      ['P' , 'port=ARG'   , 'set the port of the kitty if needed'],
+      ['h' , 'help'                , 'display this help'],
+      ['v' , 'version'             , 'show version']
+    ])              // create Getopt instance for the kitty
+    .bindHelp()     // bind option 'help' to default action
+    .parseSystem();
+
 
 module.exports = function (cb) {
-  var board    = new five.Board();
+
+  console.log(opt.options)
+  var board;
+    // if the port is specified assign it
+      board    = new five.Board({
+        port: opt.options.port
+  });
 
   board.on('ready', function() {
     var laser      = new five.Led(12)
       , onlineLed  = new five.Led(13)
       , servoX     = new five.Servo(10)
       , servoY     = new five.Servo(9)
+      ,reset
       ;
-
+      reset= function() {
+        servoX.stop();
+        servoY.stop();
+        servoX.center();
+        servoY.center();
+      };
     //
     // blinking a laser is a really bad idea
     // or maybe its awesome...
@@ -20,7 +40,8 @@ module.exports = function (cb) {
       servoX:servoX,
       servoY:servoY,
       laser:laser,
-      onlineLed: onlineLed
+      onlineLed: onlineLed,
+      reset:reset
     });
 
     //
@@ -31,28 +52,29 @@ module.exports = function (cb) {
 
     console.log('we have started our CDD (cat distraction device)');
 
-    return cb(null, 
+    return cb(null,
       { board  : board
       , x      : servoX
       , y      : servoY
       , online : onlineLed
       , laser  : laser
+      , rst    : reset
       });
 
   });
 }
 
-//           \`*-.                    
-//            )  _`-.                 
-//           .  : `. .                
-//           : _   '  \               
-//           ; *` _.   `*-._          
-//           `-.-'          `-.       
-//             ;       `       `.     
-//             :.       .        \    
-//             . \  .   :   .-'   .   
-//             '  `+.;  ;  '      :   
-//             :  '  |    ;       ;-. 
+//           \`*-.
+//            )  _`-.
+//           .  : `. .
+//           : _   '  \
+//           ; *` _.   `*-._
+//           `-.-'          `-.
+//             ;       `       `.
+//             :.       .        \
+//             . \  .   :   .-'   .
+//             '  `+.;  ;  '      :
+//             :  '  |    ;       ;-.
 //             ; '   : :`-:     _.`* ;
-//          .*' /  .*' ; .*`- +'  `*' 
-// .        `*-*   `*-*  `*-*'        
+//          .*' /  .*' ; .*`- +'  `*'
+// .        `*-*   `*-*  `*-*'
