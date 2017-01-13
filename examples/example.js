61fc26168fda9f40c4b429409201cf1ea086eb1a
gorhgorh <gorhgorh@lesnodebots.eu>
2017-01-13 15:44:22 +0000

    fixed missing deps

--- examples/example.js
+++ examples/example.js
@@ -3,7 +3,7 @@ const catbot = require('../index')
 const opts = {
   hwJoystick: true,
   boardOpt: {repl: true},
-  catConf: {hw: {laser: {pin: 13}}}
+  catConf: {hw: {laser: {pin: 12}}}
 }
 
 function catCb (err, hardware) {
